949fb6f9eb1bdd720a8b9a27f8343aeb0bb468d3
temppone <gtvtempone@gmail.com>
2021-12-09 07:10:34 -0300

    feat: @types and styled.d.ts

---
+++ src/@types/index.ts
@@ -0,0 +1,40 @@
+export interface IInputListItem {
+    id: string;
+    label: string;
+}
+
+export interface IBurger {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+}
+
+export interface IMenu {
+    onClick?: () => void;
+    open?: boolean;
+    setOpen: (open: boolean) => void;
+}
+
+export type ThemeModeType = {
+    themeName: string;
+    themeToggler: () => void;
+};
+
+export interface ISwitchButton {
+    onClick?: () => void;
+}
+
+export interface IAddress {
+    logradouro?: string;
+    bairro?: string;
+    localidade?: string;
+    uf?: string;
+    cep?: string;
+    mapsCoordinates?: {
+        lat: number;
+        lng: number;
+    };
+}
+
+export interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    name: string;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import useThemeMode from './hooks/useThemeMode';
+import { GlobalStyle } from './shared/global';
+import { darkTheme, lightTheme } from './shared/theme';
+import { ReactComponent as LogoDarkSvg } from './ui/assets/svgs/logoDark.svg';
+import { ReactComponent as LogoLightSvg } from './ui/assets/svgs/logoLight.svg';
+import Footer from './ui/components/Footer';
+import Header from './ui/components/Header';
+import Loading from './ui/components/Loading';
+
+const SearchAddress = lazy(() => import('./pages/SearchAddress'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+const Address = lazy(() => import('./pages/Address'));
+const Home = lazy(() => import('./pages/Home'));
+
+const App = (): JSX.Element => {
+    const { themeName, themeToggler } = useThemeMode();
+    const themeMode = themeName === 'light' ? lightTheme : darkTheme;
+    const logo =
+        themeName === 'light' ? (
+            <LogoDarkSvg width="50" height="50" />
+        ) : (
+            <LogoLightSvg width="50" height="50" />
+        );
+
+    return (
+        <div className="App">
+            <ThemeProvider theme={themeMode}>
+                <GlobalStyle />
+                <Router>
+                    <Header logo={logo} onClick={themeToggler} />
+                    <Suspense fallback={<Loading />}>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="*" element={<NotFound />} />
+                            <Route path="/address" element={<Address />} />
+                            <Route
+                                path="/buscar-endereco"
+                                element={<SearchAddress />}
+                            />
+                        </Routes>
+                    </Suspense>
+
+                    <Footer />
+                </Router>
+            </ThemeProvider>
+        </div>
+    );
+};
 
 export default App;
