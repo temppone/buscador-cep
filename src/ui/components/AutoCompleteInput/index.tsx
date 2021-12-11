5f61d04a74751c9b59fc454427378be51aa14e7e
temppone <gtvtempone@gmail.com>
2021-12-11 06:21:21 -0300

    feat: autocompleteInput

--- src/ui/components/AutoCompleteInput/index.tsx
+++ src/ui/components/AutoCompleteInput/index.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import Autocomplete from '@mui/material/Autocomplete';
+import * as React from 'react';
 
-interface IData {
-    id: string;
-    name: string;
+import { IOption } from '../../../@types';
+import { InputWarning } from '../Input/styles';
+import { InputContainer, AutoCompleteField, AutoCompleteLabel } from './styles';
+
+interface IAutoCompleteInput {
+    label: string;
+    options: IOption[];
+    inputError?: string;
+    onChange: any;
 }
 
-interface IAutoCompleteContainer {
-    data: any[];
-}
-
-const AutoCompleteInput = ({ data }: IAutoCompleteContainer) => {
-    return <AutoCompleteContainer />;
+const AutoCompleteInput = ({
+    inputError,
+    options,
+    label,
+    onChange,
+}: IAutoCompleteInput) => {
+    return (
+        <AutoCompleteLabel>
+            {label}
+            <Autocomplete
+                id="custom-input-demo"
+                options={options?.sort((a: IOption, b: IOption) =>
+                    a.label.localeCompare(b.label)
+                )}
+                onChange={onChange}
+                renderInput={(params) => (
+                    <InputContainer ref={params.InputProps.ref}>
+                        <AutoCompleteField {...params.inputProps} />
+                        <InputWarning>{inputError}</InputWarning>
+                    </InputContainer>
+                )}
+            />
+        </AutoCompleteLabel>
+    );
 };
 
 export default AutoCompleteInput;
