90f80715a00e1d956a31fa1714f90c935013d362
temppone <gtvtempone@gmail.com>
2021-12-12 08:41:11 -0300

    fix: eslint + cypress

---
+++ cypress/e2e/example.ts
@@ -0,0 +1,7 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Cypress TS', () => {
+    it('should change light/dark theme', () => {
+        cy.visit('http://localhost:3000')
+    })
+})
